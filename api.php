10053cae6c56371013c9e80b14aede6ece4499ee
atijust <tsujita.shou@gmail.com>
2017-09-03 04:29:29 +0900

    APIでも例外ハンドラで例外をログに出力するように

--- api.php
+++ api.php
@@ -6,6 +6,10 @@ require __DIR__ . '/vendor/autoload.php';
 call_user_func(function () {
     $container = new \Search2d\Container(__DIR__);
 
+    /** @var \Search2d\Infrastructure\Logger\ExceptionHandler $exceptionHandler */
+    $exceptionHandler = $container[\Search2d\Infrastructure\Logger\ExceptionHandler::class];
+    $exceptionHandler->install();
+
     /** @var \Search2d\Infrastructure\Presentation\Api\Frontend $frontend */
     $frontend = $container[\Search2d\Infrastructure\Presentation\Api\Frontend::class];
     $response = $frontend->handle(\Zend\Diactoros\ServerRequestFactory::fromGlobals(), new \Zend\Diactoros\Response());
